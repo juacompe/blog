db6e70a20489b2ed89463718b4096f05ae575154
juacompe <juacompe@gmail.com>
2024-06-18 15:12:29 +0700

    add coverage

--- Gemfile
+++ Gemfile
@@ -68,4 +68,5 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
   gem 'rspec-rails'
+  gem 'simplecov', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,3 +92,18 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+  add_filter '/lib/tasks/'
+  add_group 'Models', 'app/models'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Views', 'app/views'
+  add_group 'Jobs', 'app/jobs'
+  add_group 'Libraries', 'lib/'
+end
