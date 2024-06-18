8fc9403173f85db2a170d2f774284d594a112da8
juacompe <juacompe@gmail.com>
2024-06-18 15:34:16 +0700

    have rails generate rspec instead of mini test

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,15 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
