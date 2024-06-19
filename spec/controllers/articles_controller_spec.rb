6ac44b1789ac55d6220f271aed703fe9f530153f
juacompe <juacompe@gmail.com>
2024-06-19 14:32:26 +0700

    add controller tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -14,4 +14,32 @@ RSpec.describe ArticlesController, type: :controller do
       expect(response).to be_successful
     end
   end
+
+  let(:valid_attributes) {
+    { title: "Sample Article", body: "This is a sample article." }
+  }
+
+  describe "GET #show" do
+    let!(:article) { Article.create! valid_attributes }
+
+    it "returns a success response" do
+      get :show, params: { id: article.to_param }
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new Article" do
+        expect {
+          post :create, params: { article: valid_attributes }
+        }.to change(Article, :count).by(1)
+      end
+
+      it "redirects to the created article" do
+        post :create, params: { article: valid_attributes }
+        expect(response).to redirect_to(Article.last)
+      end
+    end
+  end
 end
